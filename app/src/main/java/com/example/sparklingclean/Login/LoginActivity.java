af0b821dc511305f2dc86eb32f253e83b9aa6128
Deividas <deividasliveris@rocketmail.com>
2020-05-10 15:41:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sparklingclean/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.sparklingclean.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sparklingclean.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
