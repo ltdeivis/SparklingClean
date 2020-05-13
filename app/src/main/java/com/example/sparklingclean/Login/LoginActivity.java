685b12b0da2a54dbeffed62c4e85ae6d670fb085
Gustavo <gugatomazelli@hotmail.it>
2020-05-13 11:47:33 +0100

    GUI For register screens done.

--- app/src/main/java/com/example/sparklingclean/Login/LoginActivity.java
+++ app/src/main/java/com/example/sparklingclean/Login/LoginActivity.java
@@ -2,16 +2,24 @@ package com.example.sparklingclean.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.sparklingclean.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        button  = findViewById(R.id.loginBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+            }
+        });
+    }
 }
