225c725cea9dd8c8b925e2609bdce15e0e3449ba
Deividas <deividasliveris@rocketmail.com>
2020-05-23 17:16:08 +0100

    - minor namechangez

--- app/src/main/java/com/example/sparklingclean/Firebase/DB/AppointmentHandler.java
+++ app/src/main/java/com/example/sparklingclean/Firebase/DB/AppointmentHandler.java
@@ -34,7 +34,7 @@ public class AppointmentHandler {
                             data.child("app_time").getValue().toString(), data.child("app_date").getValue().toString(),
                             data.child("employee_id").getValue().toString(), data.child("client_id").getValue().toString(),
                             data.child("notes").getValue().toString());
-                    setApp(newApp);
+                    setAppointment(newApp);
                 }
             }
 
@@ -45,7 +45,7 @@ public class AppointmentHandler {
         });
     }
 
-    public void addApp(String appTime, String appDate, String employeeId, String clientId, String notes) {
+    public void addAppointment(String appTime, String appDate, String employeeId, String clientId, String notes) {
         Appointment newApp = new Appointment(appTime, appDate, employeeId, clientId, notes);
         addAppointment(newApp);
     }
@@ -55,10 +55,10 @@ public class AppointmentHandler {
         appId = db_interface.push(refPath);
         db_interface.insert(refPath + appId, newApp);
 
-        setApp(newApp);
+        setAppointment(newApp);
     }
 
-    public void setApp(Appointment newApp) {
+    public void setAppointment(Appointment newApp) {
         this.appointment = newApp;
 
         DB_Interface db_interface = new DB_Interface();
